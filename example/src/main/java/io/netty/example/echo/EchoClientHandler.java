4a022f79e4cdc0eb0db191b974c1e95dc6057c0b
twx <twx843571091@gmail.com>
2022-01-06 14:19:26 +0800

    改写echo示例，client能输出并自动关闭

--- example/src/main/java/io/netty/example/echo/EchoClientHandler.java
+++ example/src/main/java/io/netty/example/echo/EchoClientHandler.java
@@ -17,8 +17,10 @@ package io.netty.example.echo;
 
 import io.netty.buffer.ByteBuf;
 import io.netty.buffer.Unpooled;
+import io.netty.channel.ChannelFutureListener;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.ChannelInboundHandlerAdapter;
+import io.netty.util.CharsetUtil;
 
 /**
  * Handler implementation for the echo client.  It initiates the ping-pong
@@ -41,17 +43,19 @@ public class EchoClientHandler extends ChannelInboundHandlerAdapter {
 
     @Override
     public void channelActive(ChannelHandlerContext ctx) {
+        System.out.println("channelActive..."+firstMessage.toString(CharsetUtil.UTF_8));
         ctx.writeAndFlush(firstMessage);
     }
 
     @Override
     public void channelRead(ChannelHandlerContext ctx, Object msg) {
-        ctx.write(msg);
+        System.out.println("channelRead...");
+//        ctx.write(msg);
     }
 
     @Override
     public void channelReadComplete(ChannelHandlerContext ctx) {
-       ctx.flush();
+       ctx.writeAndFlush(Unpooled.EMPTY_BUFFER).addListener(ChannelFutureListener.CLOSE);
     }
 
     @Override
